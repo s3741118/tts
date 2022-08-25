af68c590d43bb88d15e99c71a6ac0bd941036769
s3741118 <s3741118@rmit.edu.vn>
2022-08-25 10:50:29 +0700

    Update b10.py

--- b10.py
+++ b10.py
@@ -1,46 +1,46 @@
 # web tinh toan
 
-# import streamlit
-# def sum(a,b):
-#     return a+b
+import streamlit
+def sum(a,b):
+    return a+b
 
-# def minus(a,b):
-#     return a-b
+def minus(a,b):
+    return a-b
 
-# def div(a,b):
-#     return a/b
+def div(a,b):
+    return a/b
 
-# def mul(a,b):
-#     return a*b
+def mul(a,b):
+    return a*b
 
-# webName=streamlit.set_page_config(page_title="calculator")
-# numA = streamlit.number_input("The first num: ")
-# numB = streamlit.number_input("The second num: ")
-# operation=streamlit.selectbox("select", ["+","-","*", "/"])
-# if operation == "+":
-#     result = streamlit.write(sum(numA,numB))
-# elif operation == "-":
-#     result = streamlit.write(minus(numA,numB))
-# elif operation == "*":
-#     result = streamlit.write(mul(numA,numB))
-# elif operation == "/":
-#     result = streamlit.write(div(numA,numB))
+webName=streamlit.set_page_config(page_title="calculator")
+numA = streamlit.number_input("The first num: ")
+numB = streamlit.number_input("The second num: ")
+operation=streamlit.selectbox("select", ["+","-","*", "/"])
+if operation == "+":
+    result = streamlit.write(sum(numA,numB))
+elif operation == "-":
+    result = streamlit.write(minus(numA,numB))
+elif operation == "*":
+    result = streamlit.write(mul(numA,numB))
+elif operation == "/":
+    result = streamlit.write(div(numA,numB))
 
 #web chuyen doi van ban thanh giong noi
-import streamlit
-from gtts import *
-import playsound
+# import streamlit
+# from gtts import *
+# import playsound
 
-webName = streamlit.set_page_config(page_title="my web")
-webInput = streamlit.text_area(label="Input your text")
-a = webInput
-print(a)
-def convertor():
-    language = "en"
-    output=gTTS(text=webInput,lang=language)
-    filename="voice.mp3"
-    output.save(filename)
-    playsound.playsound(filename)
+# webName = streamlit.set_page_config(page_title="my web")
+# webInput = streamlit.text_area(label="Input your text")
+# a = webInput
+# print(a)
+# def convertor():
+#     language = "en"
+#     output=gTTS(text=webInput,lang=language)
+#     filename="voice.mp3"
+#     output.save(filename)
+#     playsound.playsound(filename)
 
-d=convertor()
-webButton=streamlit.button(label="save", data=d)
+# d=convertor()
+# webButton=streamlit.button(label="save", data=d)
